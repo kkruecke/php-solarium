4fad154cf7711eb104c0f9f0cff8ba843e125b7f
Kurt Krueckeberg <kurtk@pobox.com>
2015-06-11 18:56:04 -0500

    updates

--- importdb.php
+++ importdb.php
@@ -4,15 +4,15 @@ include_once("hidden/config.php");
 
   try {   
 
-     $client = new Solarium\Client($solarium_config);
+     $client = new Solarium\Client($config['solr']);
 
      $update = $client->createUpdate(); // Even initial insert is an "update"
 
      $doc = $update->createDocument();   // We insert documents 
 
-     $pdo = new \PDO("mysql:host=localhost;dbname=" . $db_config['dbname'],
-                        $db_config['dbuser'],
-                        $db_config['dbpasswd']);  
+     $pdo = new \PDO("mysql:host=localhost;dbname=" . $db_config['db']['name'],
+                        $db_config['db'['user'],
+                        $db_config['db']['pw']);  
 
      $sql = "SELECT rhs.id, rhs.signee_no, rhs.state, rhs.country, lhs.comments FROM as lhs INNER JOIN signee as rhs on lhs.id=rhs.signee_id where rhs.comments IS NOT NULL";
 
